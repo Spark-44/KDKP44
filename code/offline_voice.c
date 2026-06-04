23d2a4d00965350cad1aac63beeb4aa5fe05b660
Spark-44 <2489202283@qq.com>
2026-06-04 16:40:43 +0800

    feat: add offline voice module driver

---
+++ code/offline_voice.c
@@ -0,0 +1,196 @@
+#include "offline_voice.h"
+
+#define OFFLINE_VOICE_FRAME_LEN       (8)
+#define OFFLINE_VOICE_HEAD0           (0x6B)
+#define OFFLINE_VOICE_HEAD1_RX        (0x79)
+#define OFFLINE_VOICE_HEAD1_TX        (0x6A)
+#define OFFLINE_VOICE_MSG_RX          (0x81)
+#define OFFLINE_VOICE_MSG_TX          (0x82)
+#define OFFLINE_VOICE_TAIL            (0xFB)
+#define OFFLINE_VOICE_RX_BUF_LEN      (64)
+
+typedef enum
+{
+    OFFLINE_VOICE_STATE_IDLE = 0,
+    OFFLINE_VOICE_STATE_HEAD1,
+    OFFLINE_VOICE_STATE_DATA,
+} offline_voice_parse_state_t;
+
+#pragma section all "cpu0_dsram"
+static offline_voice_parse_state_t offline_voice_parse_state = OFFLINE_VOICE_STATE_IDLE;
+static uint8 offline_voice_frame_buf[OFFLINE_VOICE_FRAME_LEN];
+static uint8 offline_voice_frame_pos = 0;
+static uint8 offline_voice_rx_tmp[OFFLINE_VOICE_RX_BUF_LEN];
+static offline_voice_cmd_callback_t offline_voice_callback = 0;
+static void *offline_voice_user_data = 0;
+static offline_voice_stats_t offline_voice_stats = {0};
+#pragma section all restore
+
+static uint8 offline_voice_calc_checksum(const uint8 *frame)
+{
+    uint8 checksum = 0;
+    uint8 i;
+
+    for(i = 0; i < (OFFLINE_VOICE_FRAME_LEN - 2); i++)
+    {
+        checksum += frame[i];
+    }
+
+    return checksum;
+}
+
+static void offline_voice_reset_parser(void)
+{
+    offline_voice_parse_state = OFFLINE_VOICE_STATE_IDLE;
+    offline_voice_frame_pos = 0;
+}
+
+static void offline_voice_handle_frame(void)
+{
+    uint8 cmd_id;
+
+    if(offline_voice_frame_buf[7] != OFFLINE_VOICE_TAIL)
+    {
+        offline_voice_stats.bad_tail++;
+        offline_voice_reset_parser();
+        return;
+    }
+
+    if(offline_voice_frame_buf[3] != OFFLINE_VOICE_MSG_RX)
+    {
+        offline_voice_stats.bad_msg++;
+        offline_voice_reset_parser();
+        return;
+    }
+
+    if(offline_voice_calc_checksum(offline_voice_frame_buf) != offline_voice_frame_buf[6])
+    {
+        offline_voice_stats.bad_checksum++;
+        offline_voice_reset_parser();
+        return;
+    }
+
+    cmd_id = offline_voice_frame_buf[4];
+    offline_voice_stats.last_cmd_id = cmd_id;
+    offline_voice_stats.valid_frames++;
+
+    offline_voice_send_response(cmd_id);
+
+    if(offline_voice_callback != 0)
+    {
+        offline_voice_callback(cmd_id, offline_voice_user_data);
+    }
+
+    offline_voice_reset_parser();
+}
+
+void offline_voice_init(offline_voice_cmd_callback_t callback, void *user_data)
+{
+    offline_voice_callback = callback;
+    offline_voice_user_data = user_data;
+    offline_voice_reset_stats();
+    offline_voice_reset_parser();
+}
+
+void offline_voice_poll(void)
+{
+    uint32 count;
+    uint32 i;
+
+    count = debug_read_ring_buffer(offline_voice_rx_tmp, OFFLINE_VOICE_RX_BUF_LEN);
+    offline_voice_stats.bytes_total += count;
+
+    for(i = 0; i < count; i++)
+    {
+        offline_voice_feed_byte(offline_voice_rx_tmp[i]);
+    }
+}
+
+void offline_voice_feed_byte(uint8 byte)
+{
+    switch(offline_voice_parse_state)
+    {
+        case OFFLINE_VOICE_STATE_IDLE:
+            if(byte == OFFLINE_VOICE_HEAD0)
+            {
+                offline_voice_frame_buf[0] = byte;
+                offline_voice_frame_pos = 1;
+                offline_voice_parse_state = OFFLINE_VOICE_STATE_HEAD1;
+            }
+            break;
+
+        case OFFLINE_VOICE_STATE_HEAD1:
+            if(byte == OFFLINE_VOICE_HEAD1_RX)
+            {
+                offline_voice_frame_buf[1] = byte;
+                offline_voice_frame_pos = 2;
+                offline_voice_parse_state = OFFLINE_VOICE_STATE_DATA;
+            }
+            else if(byte == OFFLINE_VOICE_HEAD0)
+            {
+                offline_voice_frame_buf[0] = byte;
+                offline_voice_frame_pos = 1;
+            }
+            else
+            {
+                offline_voice_reset_parser();
+            }
+            break;
+
+        case OFFLINE_VOICE_STATE_DATA:
+            offline_voice_frame_buf[offline_voice_frame_pos] = byte;
+            offline_voice_frame_pos++;
+            if(offline_voice_frame_pos >= OFFLINE_VOICE_FRAME_LEN)
+            {
+                offline_voice_handle_frame();
+            }
+            break;
+
+        default:
+            offline_voice_reset_parser();
+            break;
+    }
+}
+
+void offline_voice_send_response(uint8 cmd_id)
+{
+    uint8 response[OFFLINE_VOICE_FRAME_LEN];
+    uint8 i;
+
+    response[0] = OFFLINE_VOICE_HEAD0;
+    response[1] = OFFLINE_VOICE_HEAD1_TX;
+    response[2] = 0x00;
+    response[3] = OFFLINE_VOICE_MSG_TX;
+    response[4] = cmd_id;
+    response[5] = 0x00;
+    response[6] = offline_voice_calc_checksum(response);
+    response[7] = OFFLINE_VOICE_TAIL;
+
+    for(i = 0; i < OFFLINE_VOICE_FRAME_LEN; i++)
+    {
+        uart_write_byte(DEBUG_UART_INDEX, response[i]);
+    }
+}
+
+void offline_voice_reset_stats(void)
+{
+    offline_voice_stats.bytes_total = 0;
+    offline_voice_stats.valid_frames = 0;
+    offline_voice_stats.bad_tail = 0;
+    offline_voice_stats.bad_checksum = 0;
+    offline_voice_stats.bad_msg = 0;
+    offline_voice_stats.last_cmd_id = 0;
+}
+
+uint8 offline_voice_get_last_cmd(void)
+{
+    return offline_voice_stats.last_cmd_id;
+}
+
+void offline_voice_get_stats(offline_voice_stats_t *stats)
+{
+    if(stats != 0)
+    {
+        *stats = offline_voice_stats;
+    }
+}
---
+++ code/offline_voice.h
@@ -0,0 +1,73 @@
+#ifndef CODE_OFFLINE_VOICE_H_
+#define CODE_OFFLINE_VOICE_H_
+
+#include "zf_common_headfile.h"
+
+#define OFFLINE_VOICE_CMD_WAKEUP               (0x01)
+#define OFFLINE_VOICE_CMD_WELCOME              (0x02)
+#define OFFLINE_VOICE_CMD_SLEEP                (0x03)
+
+#define OFFLINE_VOICE_CMD_TURN_LEFT            (0x04)
+#define OFFLINE_VOICE_CMD_TURN_RIGHT           (0x05)
+#define OFFLINE_VOICE_CMD_HIGH_BEAM            (0x06)
+#define OFFLINE_VOICE_CMD_LOW_BEAM             (0x07)
+#define OFFLINE_VOICE_CMD_FOG_LIGHT            (0x08)
+#define OFFLINE_VOICE_CMD_DOUBLE_FLASH         (0x09)
+#define OFFLINE_VOICE_CMD_INTERIOR_LIGHT       (0x0A)
+
+#define OFFLINE_VOICE_CMD_WIPER                (0x0B)
+
+#define OFFLINE_VOICE_CMD_HORN_1S              (0x0C)
+#define OFFLINE_VOICE_CMD_HORN_2S              (0x0D)
+#define OFFLINE_VOICE_CMD_HORN_3S              (0x0E)
+#define OFFLINE_VOICE_CMD_HORN_2X              (0x0F)
+#define OFFLINE_VOICE_CMD_HORN_3X              (0x10)
+#define OFFLINE_VOICE_CMD_HORN_4X              (0x11)
+#define OFFLINE_VOICE_CMD_HORN_LONG_SHORT      (0x12)
+#define OFFLINE_VOICE_CMD_HORN_RAPID           (0x13)
+#define OFFLINE_VOICE_CMD_HORN_ALARM           (0x14)
+
+#define OFFLINE_VOICE_CMD_GATE1_LEFT           (0x15)
+#define OFFLINE_VOICE_CMD_GATE1                (0x16)
+#define OFFLINE_VOICE_CMD_GATE2                (0x17)
+#define OFFLINE_VOICE_CMD_GATE3                (0x18)
+#define OFFLINE_VOICE_CMD_GATE3_RIGHT          (0x19)
+
+#define OFFLINE_VOICE_CMD_GATE1_RIGHT_BACK     (0x1A)
+#define OFFLINE_VOICE_CMD_GATE1_BACK           (0x1B)
+#define OFFLINE_VOICE_CMD_GATE2_BACK           (0x1C)
+#define OFFLINE_VOICE_CMD_GATE3_BACK           (0x1D)
+#define OFFLINE_VOICE_CMD_GATE3_LEFT_BACK      (0x1E)
+
+#define OFFLINE_VOICE_CMD_FORWARD_10M          (0x1F)
+#define OFFLINE_VOICE_CMD_BACKWARD_10M         (0x20)
+#define OFFLINE_VOICE_CMD_SNAKE_FORWARD        (0x21)
+#define OFFLINE_VOICE_CMD_SNAKE_BACKWARD       (0x22)
+#define OFFLINE_VOICE_CMD_CCW_CIRCLE           (0x23)
+#define OFFLINE_VOICE_CMD_CW_CIRCLE            (0x24)
+#define OFFLINE_VOICE_CMD_TURN_LEFT_DRIVE      (0x25)
+#define OFFLINE_VOICE_CMD_TURN_RIGHT_DRIVE     (0x26)
+
+#define OFFLINE_VOICE_CMD_MAX                  (0x26)
+
+typedef void (*offline_voice_cmd_callback_t)(uint8 cmd_id, void *user_data);
+
+typedef struct
+{
+    uint32 bytes_total;
+    uint32 valid_frames;
+    uint32 bad_tail;
+    uint32 bad_checksum;
+    uint32 bad_msg;
+    uint8  last_cmd_id;
+} offline_voice_stats_t;
+
+void offline_voice_init(offline_voice_cmd_callback_t callback, void *user_data);
+void offline_voice_poll(void);
+void offline_voice_feed_byte(uint8 byte);
+void offline_voice_send_response(uint8 cmd_id);
+void offline_voice_reset_stats(void);
+uint8 offline_voice_get_last_cmd(void);
+void offline_voice_get_stats(offline_voice_stats_t *stats);
+
+#endif
